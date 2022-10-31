2db26b21a7044ccf44d20648d9a65e52f51de2db
Ginny-Dang <ginnydang0728@gmail.com>
2022-10-30 20:34:12 -0500

    Add lesson 13

--- src/main/java/io/datajek/springdatajpa/tennisplayer/PlayerRepository.java
+++ src/main/java/io/datajek/springdatajpa/tennisplayer/PlayerRepository.java
@@ -10,4 +10,41 @@ import org.springframework.stereotype.Repository;
 public class PlayerRepository {
     @PersistenceContext
     EntityManager entityManager;
+
+    /**
+     * Insert new Player to the table
+     * @param player
+     * @return a Player object which has been inserted
+     */
+    public Player insertPlayer(Player player) {
+        return entityManager.merge(player);
+    }
+
+    /**
+     * Update an existing Player in the table
+     * @param player
+     * @return a Player object which has been updated
+     */
+    public Player updatePlayer(Player player) {
+        return entityManager.merge(player);
+    }
+
+    /**
+     * Find an existing Player with the given id
+     * @param id
+     * @return
+     */
+    public Player getPlayerById(int id) {
+        return entityManager.find(Player.class, id);
+    }
+
+    /**
+     * Delete an existing Player with the given id
+     * @param id
+     */
+    public void deletePlayerById(int id){
+        Player player = entityManager.find(Player.class, id);
+        entityManager.remove(player);
+    }
+
 }
